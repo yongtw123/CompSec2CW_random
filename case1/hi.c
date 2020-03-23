3177c25e9ff50b6c82fafea30e1b5dd8b226c441
Yen-Chung Tseng <ucabyts@ucl.ac.uk>
2020-03-23 07:17:06 +0000

    case1 wip, core dump not working as expected

--- case1/hi.c
+++ case1/hi.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 int main(void)
 {
   printf("Hello world!\n");
+  pid_t pid = getpid();
+  printf("pid: %d\n", pid);
+
+  //trigger divide by 0
   printf("1 over 0 is %d", 1/0);
+
   return 0;
 }
