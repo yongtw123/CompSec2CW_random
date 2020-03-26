5906e29d43bc4e19c53a3d281b77e7c807cc2201
naum97 <naum.anteski.16@ucl.ac.uk>
2020-03-26 14:15:51 +0100

    better amplification case3

--- case3/task.sh
+++ case3/task.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 SECTORUN=30
-ITER=1022
 STOPTIME=$(($(date +%s) + SECTORUN))
+USERNAME="testuser"
+
+# Create a dummy user
+
+id -u $USERNAME || \
+ (echo "testuser:x:1001:1001:,,,:/home/testuser:/bin/bash" \
+ | newusers)
+
+# Switch into the user and exit in a loop
+# Should trigger the journald logging process in the host
+
 while [[ $(date +%s) -lt $STOPTIME ]]; do
- echo "testuser$ITER:x:$ITER:$ITER:,,,:/home/testuser$ITER:/bin/bash" | newusers && su - testuser$ITER -c "whoami && exit" && (( ITER++))
+ su - testuser -c "whoami && exit"&
 done;
